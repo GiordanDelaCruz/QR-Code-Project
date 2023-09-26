9d68f6d8c6fe5b49f1e3913dda9259ece4ff741a
Giordan Dela Cruz <52592435+GiordanDelaCruz@users.noreply.github.com>
2023-09-26 13:45:20 -0400

    Updated index.js to create a input log history, orgranized code, and documented it.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 */
 
 // Import Dependencies
+import { timeStamp } from "console";
 import fs from "fs";
 import inquirer from "inquirer";
 import qr from "qr-image";
@@ -14,23 +15,72 @@ const question = [
     {
         "type": 'input',
         "name": 'URL',
-        "message": "Please enter an url:"
+        "message": "Please enter an url to be converted into a QR code:"
     }
 ];
 
 // Output
-inquirer.prompt(question).then( (answers) =>{
-    console.log(JSON.stringify(answers, null, ' '));
-
-    // Save user input in file
-    fs.writeFile("inputs.txt", answers.URL, err => {
-
-        if(err){
-            console.log("Unable to create text file.")
-        }
-    });
-
-    // Turn user input in QR-image
-    var qr_png = qr.image(answers.URL, {type: 'png'});
-    qr_png.pipe( fs.createWriteStream("user_input_qr.png"));
+inquirer.prompt(question).then( (answer) =>{
+    var stamps = generateStamps();
+    saveInputLog(answer, stamps);
+    createQR(answer, stamps);
 });
+
+/** This function will create time and date stamps the will be used for the 
+ *  input log history and qr codes.
+ * 
+ * @returns array of time and data stamps
+ */
+function generateStamps(){
+    const date = new Date();
+    var timeObj = {
+        hour : date.getHours(),
+        minute : date.getMinutes(), 
+        second : date.getSeconds()
+    }
+    var dateObj = {
+        day : date.getDate(),
+        month : date.getMonth() + 1,
+        year : date.getFullYear()
+    }
+
+    const stamps = [
+        `ti_${timeObj.hour}_${timeObj.minute}_${timeObj.second}_dt_${dateObj.day}_${dateObj.month}_${dateObj.year}`,
+        `${timeObj.hour}:${timeObj.minute}:${timeObj.second}`, 
+        `${dateObj.day}:${dateObj.month}:${dateObj.year}`     
+    ]
+
+    return stamps;
+}
+/** This function will save a log of all the inputs from the user.
+ * 
+ * @param {*} answer 
+ * @param {*} stamps 
+ */
+function saveInputLog(answer, stamps){
+    fs.readFile('./input_log.txt', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        var textHistory = data;
+
+        // Save user input in file
+        var updateText = textHistory + "\n" + answer.URL + " | " + stamps[1] + " | " + stamps[2];
+        fs.writeFile("input_log.txt", updateText, err => {
+            if(err){
+                console.log("Unable to create text file.")
+            }
+        });
+    });
+}
+/** This function will create a QR code based on the user input.
+ * 
+ * @param {*} answer 
+ * @param {*} stamps 
+ */
+function createQR(answer, stamps){
+    var qr_png = qr.image(answer.URL, {type: 'png'});
+    var fileName = "qr_code_" + stamps[0] + ".png"
+    qr_png.pipe( fs.createWriteStream("./qr-code-images/" + fileName));
+}
